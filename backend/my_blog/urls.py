69cfd63052c142c0119e549ac2384cbf33f0bb15
Yosuke Santamaria <youman.318@gmail.com>
2023-10-22 14:32:35 +0900

    login

--- backend/my_blog/urls.py
+++ backend/my_blog/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from views import fetch_users_list
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 
 urlpatterns = [
     path('login/',
@@ -25,4 +26,5 @@ urlpatterns = [
          ),
          name='login'),
     path('logout/', LogoutView.as_view, name='logout'),
+    path('', fetch_users_list, name='top'),
 ]
--- backend/my_blog/views.py
+++ backend/my_blog/views.py
@@ -1,34 +1,13 @@
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.contrib.auth import get_user_model
 
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect('/dashboard/')  # ログイン後のリダイレクト先
-        else:
-            return render(request, 'login.html', {'error_message': 'Invalid credentials'})
-    return render(request, 'login.html')
+def fetch_users_list(request):
+    user = get_user_model()
 
+    users = user.objects.all()
 
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect('/login/')  # ログアウト後のリダイレクト先
+    context = {'users': users}
 
+    return render(request, 'top.html', context)
 
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-
-class CustomRegistrationView(CreateView):
-    template_name = 'register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
