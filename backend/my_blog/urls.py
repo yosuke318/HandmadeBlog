2d165e7568e9cba88b95f1d3d551b1011c160bf4
Yosuke Santamaria <youman.318@gmail.com>
2023-10-28 09:38:30 +0900

    login

--- backend/my_blog/urls.py
+++ backend/my_blog/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from views import fetch_users_list
+from . import views
+from .views import fetch_users_list, register
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
@@ -25,6 +25,10 @@ urlpatterns = [
              template_name='login.html'
          ),
          name='login'),
-    path('logout/', LogoutView.as_view, name='logout'),
+    path('logout/',
+         LogoutView.as_view(
+             template_name='logout.html'
+         ), name='logout'),
     path('', fetch_users_list, name='top'),
+    path('register/', views.register, name='register'),
 ]
--- backend/my_blog/views.py
+++ backend/my_blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+
 from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 def fetch_users_list(request):
@@ -10,4 +13,19 @@ def fetch_users_list(request):
     context = {'users': users}
 
     return render(request, 'top.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        # POSTリクエストの場合、フォームデータを処理
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # フォームが有効な場合、ユーザーを作成しログイン
+            user = form.save()
+            login(request, user)
+            return redirect('top')  # ユーザーをホームページにリダイレクト
+    else:
+        # GETリクエストの場合、新しいフォームを表示
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
